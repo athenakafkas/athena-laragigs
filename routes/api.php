f7444fc6c119597a4a53ff5635f98b768e6b33bb
Athena Kafkas <athenakafkas2021@gmail.com>
2023-08-13 13:43:42 -0500

    first commit

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//the following route is accessible through (http://127.0.0.1:8000/api/posts)
+/*access the URL path /posts using an HTTP GET request >> return a JSON response containing an array of posts.
+Route::get('/posts', function() {
+    return response()->json([
+        'posts' => [
+            [
+                'title' => 'Post One'
+            ]
+        ]
+    ]);
+});
+*/
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+//bring in model
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,37 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
 });
+
+//Single Listing 
+Route::get('/listings/{id}', function($id) {
+    return view ('listing', [
+        'listing' => Listing::find($id)
+    ]);
+});
+
+/* NOTES on ROUTES for REFERENCE:
+//welcome route
+Route::get('/hello', function () {
+    return response('<h1>Hello World</h1>', 200)
+        ->header('Content-Type', 'text/html')
+        ->header('foo', 'bar');
+});
+
+//posts route
+Route::get('/posts/{id}', function ($id) {
+    dd($id);
+    return response('Post ' . $id);
+})->where('id', '[0-9]+');
+
+//able to make new search request (http://127.0.0.1:8000/search?name=Athena&city=Madison)
+//pair with use Illuminate\Http\Request; ... download Laravel Blade Snippets to "import class" on Request
+Route::get('search', function(Request $request){
+    return $request->name . ' ' . $request->city;
+    //return request as plain text^^
+});
+*/
