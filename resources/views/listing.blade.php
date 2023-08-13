db0b953c338f0a0c00b5beb8cf15dc5c4cfc27fa
Athena Kafkas <athenakafkas2021@gmail.com>
2023-08-13 13:44:58 -0500

    first layers of Laragigs site

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,2 +1,6 @@
-<h2>{{ $listing['title'] }}</h2>
-<p>{{ $listing['description'] }}</p>
+@if (!is_null($listing))
+    <h2>{{ $listing['title'] }}</h2>
+    <p>{{ $listing['description'] }}</p>
+@else
+    <p>Listing not available.</p>
+@endif
